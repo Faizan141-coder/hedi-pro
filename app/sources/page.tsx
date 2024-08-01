915b11cfc5d2e6ef4da4bf68d3a6b480e9cf42a4
Muhammad Faizan Hasnaat <faizanhasnaat13@gmail.com>
2024-08-01 23:00:14 +0500

    commit

--- app/sources/page.tsx
+++ app/sources/page.tsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const SourcesPage = () => {
   return (
-    <div>SourcesPage</div>
+    <div>
+      <div className='border border-[#9747ff] border-dashed rounded-full w-[500px] h-[500px]'>
+        <div className='border border-[#3ea381] bg-[#3ea381] rounded-full bg w-[42%] h-[42%]'>
+          
+        </div>
+        <div className='border border-[#a33e60] bg-[#a33e60] rounded-full bg w-[15%] h-[15%]'>
+
+        </div>
+        <div className='border border-[#a33e60] bg-[#a33e60] rounded-full bg w-[20%] h-[20%]'>
+
+        </div>
+        <div className='border border-[#3ea381] bg-[#3ea381] rounded-full bg w-[10%] h-[10%]'>
+
+        </div>
+        <div className='border border-[#a33e60] bg-[#a33e60] rounded-full bg w-[12%] h-[12%]'>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
